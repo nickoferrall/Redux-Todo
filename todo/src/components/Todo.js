328ef190ed8f4565e1e683e33f0c6ae4c3fde615
Nick O'Ferrall <nickoferrall@gmail.com>
2018-12-03 17:24:02 -0500

    Initial Commit

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,25 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import * as actionTypes from '../../store/actions';
+import React from 'react';
+// import React, { Component } from 'react';
+
+// class Todo extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 Heyyyy
+//             </div>
+//         );
+//     }
+// }
+
+// export default connect()(Todo);
+
+const Todo = () => (
+    <div>
+        Heyy
+    </div>
+);
+
+export default Todo;
